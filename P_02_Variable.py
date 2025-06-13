ae9dd5866a851231b8cf14de84c5fd102dd8844e
HossainH <hossainh50890@masseyhigh.school.nz>
2025-06-13 12:05:55 +1200

    02_Variable_Basics

---
+++ P_02_Variable.py
@@ -0,0 +1,25 @@
+#Code to learn about variables
+
+#Variables can be a string (Note the speech marks)
+greeting = "hello world"
+
+#Variables can be a number
+fav_num = 7
+num_str = "5"
+#Variables can be boolean
+love_programming = True
+
+#This variable (used to have a problem)!
+
+My_name = "Miss G"
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+print(fav_num*2)
+print(num_str*5)
+
+#Uppercase reserved for constants
+PI = 3.14
+#variables cant be started with letters
+a_01_variable= "can be anything here"
