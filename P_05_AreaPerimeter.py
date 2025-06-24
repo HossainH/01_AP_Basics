e01441737265c47c6c800f8c69dfd66916ba7c61
HossainH <hossainh50890@masseyhigh.school.nz>
2025-06-24 12:01:10 +1200

    Delete P_05_AreaPerimeter.py

--- P_05_AreaPerimeter.py
+++
@@ -1,8 +0,0 @@
-width = float(input("Enter the width"))
-height = float(input("Enter the height"))
-
-area = width * height
-perimeter = 2 * (width + height)
-
-print(f"the area is {area} units")
-print(f"the perimeter is {perimeter} units")
